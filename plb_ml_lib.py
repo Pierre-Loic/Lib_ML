24a440cdc3e65f1088760b288bb8ec962311cbac
__plb__ <plbayart@gmail.com>
2020-11-16 22:39:35 +0100

    create gradient descent function

--- plb_ml_lib.py
+++ plb_ml_lib.py
@@ -16,21 +16,47 @@ class LinearRgressionNp:
             self._fit_normal(X, y)
         elif self.solver == "pseudo_inv":
             self._fit_pseudo_inv(X, y)
+        elif self.solver == "ols":
+            self._fit_ols(X, y)
+        elif self.solver == "gd":
+            self._fit_gd(X, y)
+        elif self.solver == "sgd":
+            pass
+        elif self.solver == "bgd":
+            pass
         else:
             print(f"Solver {self.solver} non reconnu")
             return
         self._update_parameters()
     
     def predict(self, X):
-        X_1 = np.c_[np.ones((X.shape[0], 1)), X]
+        X_1 = self._add_constant(X)
         return X_1.dot(self.theta)
 
+    def _add_constant(self, X):
+        return np.c_[np.ones((X.shape[0], 1)), X]
+
+    # Fit functions
     def _fit_normal(self, X, y):
-        X_1 = np.c_[np.ones((X.shape[0], 1)), X]
+        X_1 = self._add_constant(X)
         self.theta = np.linalg.inv(X_1.T.dot(X_1)).dot(X_1.T.dot(y))
 
     def _fit_pseudo_inv(self, X, y):
-        pass
+        X_1 = self._add_constant(X)
+        self.theta = np.linalg.pinv(X_1).dot(y)
+
+    def _fit_ols(self, X, y):
+        X_1 = self._add_constant(X)
+        self.theta = np.linalg.lstsq(X_1, y, rcond=1e-6)[0]
+
+    def _fit_gd(self, X, y, learning_rate=0.01, n_iter=10000):
+        X_1 = self._add_constant(X)
+        y = y.reshape(-1,1)
+        self.theta = np.random.randn(X_1.shape[1], 1)
+        for i in range(n_iter):
+            gradient = (2/X_1.shape[0])*X_1.T.dot(X_1.dot(self.theta)-y)
+            self.theta = self.theta - learning_rate*gradient
+        self.theta = self.theta.flatten()
 
     def _update_parameters(self):
         self.intercept_ = self.theta[0]
--- test_plb_ml_lib.py
+++ test_plb_ml_lib.py
@@ -5,11 +5,12 @@ import plb_ml_lib as pml
 
 def test_LNNp_normal():
     reg_ref = LinearRegression()
-    reg_test = pml.LinearRgressionNp()
     X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]]) 
     y = np.dot(X, np.array([1, 2])) + 3*np.random.rand() # y = 1 * x_0 + 2 * x_1 + 3
     reg_ref.fit(X, y)
-    reg_test.fit(X, y)
-    np.testing.assert_array_almost_equal(reg_ref.coef_, reg_test.coef_)
-    np.testing.assert_array_almost_equal(reg_ref.intercept_, reg_test.intercept_)
-    np.testing.assert_array_almost_equal(reg_ref.predict(np.array([[3, 5]])), reg_test.predict(np.array([[3, 5]])))
+    for solver in ["normal_eq", "pseudo_inv", "ols", "gd"]:
+        reg_test = pml.LinearRgressionNp(solver)
+        reg_test.fit(X, y)
+        np.testing.assert_array_almost_equal(reg_ref.coef_, reg_test.coef_)
+        np.testing.assert_array_almost_equal(reg_ref.intercept_, reg_test.intercept_)
+        np.testing.assert_array_almost_equal(reg_ref.predict(np.array([[3, 5]])), reg_test.predict(np.array([[3, 5]])))
