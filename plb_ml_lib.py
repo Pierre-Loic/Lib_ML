71fbdb40edc0df5fedefc0037f9f3d196eef30e8
__plb__ <plbayart@gmail.com>
2020-11-20 19:24:12 +0100

    add sgd for linear regression and Ridge regression

--- plb_ml_lib.py
+++ plb_ml_lib.py
@@ -3,7 +3,7 @@ import numpy as np
 class LinearRegressionPy:
     pass
 
-class LinearRgressionNp:
+class LinearRegressionNp:
     
     def __init__(self, solver="normal_eq"):
         self.solver = solver
@@ -21,7 +21,7 @@ class LinearRgressionNp:
         elif self.solver == "gd":
             self._fit_gd(X, y)
         elif self.solver == "sgd":
-            pass
+            self._fit_sgd(X, y)
         elif self.solver == "bgd":
             pass
         else:
@@ -58,11 +58,60 @@ class LinearRgressionNp:
             self.theta = self.theta - learning_rate*gradient
         self.theta = self.theta.flatten()
 
+    def _fit_sgd(self, X, y, t0=800, lr0=0.1, n_epochs=500):
+        X_1 = self._add_constant(X)
+        y = y.reshape(-1,1)
+        self.theta = np.random.randn(X_1.shape[1], 1)
+        for epoch in range(n_epochs):
+            random_index = np.random.randint(X_1.shape[0])
+            X_i = X_1[random_index:random_index+1]
+            y_i = y[random_index:random_index+1]
+            gradient = 2*X_i.T.dot(X_i.dot(self.theta)-y_i)
+            learning_rate = lr0*(t0/(t0+epoch))
+            self.theta = self.theta - learning_rate*gradient
+        self.theta = self.theta.flatten()
+
+    def _fit_bgd(self, X, y, learning_rate=0.01, n_iter=10000):
+        pass
+
     def _update_parameters(self):
         self.intercept_ = self.theta[0]
         self.coef_ = self.theta[1:]
 
-    def _check_input(self, X, y):
-        """ Check the size and the types of the data """
-        pass
+class RidgeNp:
 
+    def __init__(self, solver="normal_eq", alpha=1):
+        self.solver = solver
+        self.alpha = alpha
+        self.theta = None
+        self.intercept_ = None
+        self.coef_ = None
+
+    def fit(self, X, y):
+        if self.solver == "normal_eq":
+            self._fit_normal(X, y)
+        elif self.solver == "gd":
+            pass
+        elif self.solver == "sgd":
+            pass
+        elif self.solver == "bgd":
+            pass
+        else:
+            print(f"Solver {self.solver} non reconnu")
+            return
+        self._update_parameters()
+
+    def predict(self, X):
+        X_1 = self._add_constant(X)
+        return X_1.dot(self.theta)
+
+    def _fit_normal(self, X, y):
+        X_1 = self._add_constant(X)
+        self.theta = np.linalg.inv(X_1.T.dot(X_1)+self.alpha*np.identity(X_1.shape[1])).dot(X_1.T.dot(y))
+
+    def _add_constant(self, X):
+        return np.c_[np.ones((X.shape[0], 1)), X]
+
+    def _update_parameters(self):
+        self.intercept_ = self.theta[0]
+        self.coef_ = self.theta[1:]
--- test_plb_ml_lib.py
+++ test_plb_ml_lib.py
@@ -1,16 +1,30 @@
 import pytest
 import numpy as np
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge
 import plb_ml_lib as pml
 
-def test_LNNp_normal():
+@pytest.mark.parametrize("solver", ["normal_eq", "pseudo_inv", "ols", "gd", "sgd"])
+def test_LNNp_normal(solver):
     reg_ref = LinearRegression()
     X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]]) 
     y = np.dot(X, np.array([1, 2])) + 3*np.random.rand() # y = 1 * x_0 + 2 * x_1 + 3
     reg_ref.fit(X, y)
-    for solver in ["normal_eq", "pseudo_inv", "ols", "gd"]:
-        reg_test = pml.LinearRgressionNp(solver)
-        reg_test.fit(X, y)
-        np.testing.assert_array_almost_equal(reg_ref.coef_, reg_test.coef_)
-        np.testing.assert_array_almost_equal(reg_ref.intercept_, reg_test.intercept_)
-        np.testing.assert_array_almost_equal(reg_ref.predict(np.array([[3, 5]])), reg_test.predict(np.array([[3, 5]])))
+    reg_test = pml.LinearRegressionNp(solver)
+    reg_test.fit(X, y)
+    np.testing.assert_array_almost_equal(reg_ref.coef_, reg_test.coef_, decimal=2)
+    np.testing.assert_array_almost_equal(reg_ref.intercept_, reg_test.intercept_, decimal=2)
+    np.testing.assert_array_almost_equal(reg_ref.predict(np.array([[3, 5]])), reg_test.predict(np.array([[3, 5]])), decimal=2)
+
+@pytest.mark.parametrize("alpha", [0.001, 0.01, 0.1, 1, 10, 100, 1000])
+def test_RidgeNp_normal(alpha):
+    n_samples, n_features = 10000, 5
+    y_train = np.random.randn(n_samples)
+    X_train = np.random.randn(n_samples, n_features)
+    ridge_ref = Ridge(alpha=alpha)
+    ridge_test = pml.RidgeNp("normal_eq", alpha=alpha)
+    ridge_ref.fit(X_train, y_train)
+    ridge_test.fit(X_train, y_train)
+    X_test = np.random.randn(n_samples, n_features)
+    np.testing.assert_array_almost_equal(ridge_ref.coef_, ridge_test.coef_, decimal=3)
+    np.testing.assert_array_almost_equal(ridge_ref.intercept_, ridge_test.intercept_, decimal=3)
+    np.testing.assert_array_almost_equal(ridge_ref.predict(X_test), ridge_test.predict(X_test), decimal=3)
